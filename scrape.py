029b235b12695fa44dee00ca91a4fe20624f102b
Julian Harris <julian.harris@gmail.com>
2024-12-26 19:35:51 +0000

    Tweaks

--- scrape.py
+++ scrape.py
@@ -1,9 +1,13 @@
 from curl_cffi import requests 
 from pathlib import Path
+import sys
+import re
+
 def get_tripadvisor_homepage():
     """Gets the contents of the tripadvisor homepage using curl_cffi."""
     try:
         # Use impersonate to mimic a real browser
+        # tripadvisor has notoriously powerful anti-scraping measures but this seems to work
         response = requests.get("https://www.tripadvisor.com/", impersonate="chrome")
         return response.content
     except Exception as e:
@@ -23,6 +27,11 @@ def save_to_file(content, filepath):
         print(f"Error saving content to file: {e}")
 
 if __name__ == "__main__":
-    content = get_tripadvisor_homepage()
+    if len(sys.argv) < 2:
+        print("Usage: python scrape.py <url>")
+        sys.exit(1)
+    url = sys.argv[1]
+    # domain = re.search(r'https?://([^/]+)', url).group(1)
+    content = requests.get(url, impersonate="chrome").content
     if content:
-        save_to_file(content, Path("output/tripadvisor/index.html"))
+        save_to_file(content, Path(f"output/{url.replace('https://', '').replace('http://', '')}/index.html"))
